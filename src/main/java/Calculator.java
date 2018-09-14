c9ea2c629efbeac4915bee369797ecaa9abf9dfd
Pedro Piloto <pedro.piloto@talkdesk.com>
2018-09-14 14:19:41 +0100

    step 1 - 3

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,9 +1,30 @@
+import javax.sound.sampled.SourceDataLine;
 
 /**
  * Calculator
  */
 public class Calculator {
   public int Add(String numbers) {
-    throw new UnsupportedOperationException();
+    if(numbers == ""){
+      return 0;
+    }
+    String delimiter = ",|\\n";
+    String delimiterNumbersArray[] = numbers.split(delimiter);
+
+    String p = delimiterNumbersArray[0].replace("//", "");
+    System.out.println("P: " + p);
+
+    delimiter +=  '|' + p;
+
+    System.out.print("Delimiter: " + delimiter);
+
+    int total = 0;
+    for(int i = 0; i < delimiterNumbersArray.length; i++) {
+      if(!delimiterNumbersArray[i].startsWith("//")){
+        System.out.println("number: " + delimiterNumbersArray[i]);
+      total += Integer.parseInt(delimiterNumbersArray[i]);
+      }
+    }
+    return total;
   }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -5,5 +5,63 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CalculatorTest {
-    /*added comment*/
+    @Test 
+    public void testEmptyString() {
+        //Arranje
+        Calculator calculator = new Calculator();
+        //Act
+        int number = calculator.Add("");
+        //Assert
+        assertEquals(number, 0);
+    }
+
+    @Test
+    public void testOneNumberString() {
+        // Arrange
+        Calculator calculator = new Calculator();
+        // Act
+        int number = calculator.Add("3");
+        // Assert
+        assertEquals(number, 3);
+    }
+
+    @Test
+    public void testTwoNumberString() {
+        // Arrange
+        Calculator calculator = new Calculator();
+        // Act
+        int number = calculator.Add("2,3");
+        // Assert
+        assertEquals(number, 5);
+    }
+
+    @Test
+    public void testUnkownNumberString() {
+        // Arrange
+        Calculator calculator = new Calculator();
+        // Act
+        int number = calculator.Add("2,3,4,5,6");
+        // Assert
+        assertEquals(number, 20);
+    }
+
+    @Test
+    public void testCommaOrNewLineNumberString() {
+        // Arrange
+        Calculator calculator = new Calculator();
+        // Act
+        int number = calculator.Add("1,2\n3");
+        // Assert
+        assertEquals(number, 6);
+    }
+
+    @Test
+    public void testDelimitersString() {
+        // Arrange
+        Calculator calculator = new Calculator();
+        // Act
+        int number = calculator.Add("//;\n1,2\n3");
+        // Assert
+        assertEquals(number, 6);
+    }
 }
