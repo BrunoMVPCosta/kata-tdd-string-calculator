1433e0544b8dc0750d345575a12210ea77af95b5
Carlos Alves <carlos.alves@OPO-MBP-0088.local>
2018-09-14 14:41:55 +0100

    Step 1 to 6

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,9 +1,45 @@
+import java.util.ArrayList;
 
 /**
  * Calculator
  */
 public class Calculator {
   public int Add(String numbers) {
-    throw new UnsupportedOperationException();
+    String[] parts;
+    if(numbers == "")
+      return 0;
+    if(numbers.contains("/")){
+      char newDelimiter = numbers.charAt(2);
+      String[] aux = numbers.split("\n"); 
+      parts = aux[1].split(""+ newDelimiter); 
+    } else {
+      parts = numbers.split(",|\\n");
+    }
+    int sum = 0;
+    ArrayList<Integer> negatives = new ArrayList<Integer>();
+    int j = 0;
+    for(int i = 0; i < parts.length; i++) {
+      if(parts[i] == "")
+        return sum;
+      else if(Integer.parseInt(parts[i]) < 0){
+        negatives.add(j, Integer.parseInt(parts[i]));
+        j++;
+        continue;
+      } else if(Integer.parseInt(parts[i]) > 1000)
+        continue;
+
+      sum += Integer.parseInt(parts[i]);
+    }
+    if(negatives.size() > 0){
+      String total_negatives = "";
+        for (int n : negatives) {
+          total_negatives = total_negatives+" " + n;
+        }
+
+
+      throw new IllegalArgumentException("negatives not allowed:" + total_negatives);
+    }
+
+    return sum;
   }
 }
